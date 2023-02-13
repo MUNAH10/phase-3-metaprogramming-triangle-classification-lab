de6a0c26cc65d37fa6748a60b6dd6845fb1f9811
MUNAH10 <munahassan5075@gmail.com>
2023-02-13 15:52:03 +0300

    completed

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,28 @@
 class Triangle
-  # write code here
+  
+  attr_accessor :side1, :side2, :side3
+
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+
+    raise TriangleError, "All sides must have a length greater than 0" if side1 <= 0 || side2 <= 0 || side3 <= 0
+    raise TriangleError, "This is not a valid triangle" if (side1 + side2 <= side3) || (side1 + side3 <= side2) || (side2 + side3 <= side1)
+
+  end
+  
+  def kind
+    if side1 == side2 && side2 == side3
+      return :equilateral
+    elsif side1 == side2 || side1 == side3 || side2 == side3
+      return :isosceles
+    else
+      return :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+    
+  end
 end
